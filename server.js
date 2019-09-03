e44d139d28a6b5aa25f5b2563854acff57ca1a34
chikechris <cchike2017@gmail.com>
2019-09-03 14:19:48 -0400

    added more to server.js file

--- server.js
+++ server.js
@@ -6,7 +6,8 @@
 const express = require('express')
 
 const server = express()
-
+let movieId = 5
+let actorId = 2
 let actors = [
   {
     id: 1,
@@ -41,19 +42,34 @@ let movies = [
   }
 ]
 
-
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'is up and running' })
 })
 
 server.get('/api/movies', (req, res) => {
+  const minRating = req.query.minrating
+
+  let result = [...movies]
+  if (minRating) {
+    result = movies.filter(m => m.rating >= minrating)
+  }
   res.status(200).json(movies)
 })
 server.post('/api/movies', (req, res) => {
   const movie = req.body // grab data from body
+
+  // add new id
+  movie.id = movieId++
   movies.push(movie) // add to store
   res.status(201).json(movies) // return data
 })
 
+server.delete('/api/movies/:id', (req, res) => {
+  const id = req.params.id
+
+  movies = movies.filter(m => m.id !== Number(id))
+
+  res.status(200).json(movies)
+})
 
 module.exports = server
